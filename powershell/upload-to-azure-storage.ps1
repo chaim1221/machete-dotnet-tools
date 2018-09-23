597ea18a65b7d4b0ecabc6cafdf6a7285b2e5827
Chaim Eliyah <chaimeliyah@gmail.com>
2018-09-23 03:27:30 -0700

    upload successful

--- powershell/upload-to-azure-storage.ps1
+++ powershell/upload-to-azure-storage.ps1
@@ -1,1 +1,26 @@
 # DO ALL THE THINGS!
+$containerName = "handycontainer"
+$resourceGroupName = "macheteTest"
+$storageAccountName = "fuckingazurestoragebro"
+$file = "nonsense.zip"
+$path = ".\payloads\"
+
+try {
+
+$context = (Get-AzureRmStorageAccount -Name $storageAccountName `
+  -ResourceGroupName $resourceGroupName).Context
+
+Set-AzureStorageBlobContent -File (Join-Path $path $file) `
+  -Container $containerName `
+  -Blob $file `
+  -Context $context
+
+Write-Host "Success"
+
+} catch {
+
+Write-Host -ForegroundColor Yellow "Well, that didn't work very well."
+Write-Host -ForegroundColor Yellow "Exception message:"
+Write-Host -ForegroundColor Red $_.Exception.Message
+
+}
