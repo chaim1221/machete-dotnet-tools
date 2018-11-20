eaceea04f8cc344e81d1b20fb6426501d5ce41dd
Chaim Eliyah <chaimeliyah@gmail.com>
2018-11-20 14:31:24 -0600

    setting up the dev sql container

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,5 +1,12 @@
 sudo docker pull mcr.microsoft.com/mssql/server
 sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passw0rD!' -p 1433:1433 \
   --name sql1 -d mcr.microsoft.com/mssql/server:2017-latest
-
-
+sudo docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost \
+  -U SA -P 'passw0rD!' -Q 'CREATE USER dev WITH PASSWORD 'passw0rD!'
+sudo docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost \
+  -U SA -P 'passw0rD!' \
+  -Q 'EXEC sys.sp_addsrvrolemember @loginame = N'dev', @rolename = N'sysadmin';'
+sudo docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost \
+  -U SA -P 'passw0rD!' \
+  -Q 'ALTER SERVER ROLE [sysadmin] ADD MEMBER [dev]'
+# docker exec -it sql1 bash
